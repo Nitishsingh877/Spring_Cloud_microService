c5c79509a439ec220122e4b49677921898aae7d9
Nitish singh <nitishkumarsingh877@gmail.com>
2025-03-11 22:27:49 +0530

    added rest template in catalog

--- Movie_Catalog_Service/src/main/java/resource/MovieCatalogResource.java
+++ Movie_Catalog_Service/src/main/java/resource/MovieCatalogResource.java
@@ -1,22 +1,44 @@
 package resource;
 
 import models.CatalogItem;
-import org.springframework.web.bind.annotation.GetMapping;
+import models.Movie;
+import models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("ye dil mange more","Romance",4)
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        // Movie movie = restTemplate.getForObject("http://localhost:8081/movies/foo", Movie.class);
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("2345", 3),
+                new Rating("1258", 5)
         );
+
+        return ratings.stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+                    return  new CatalogItem(movie.getTitle(), "description", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+
+//        return Collections.singletonList(
+//                new CatalogItem("ye dil mange more","Romance",4)
+//        );
     }
 }
--- Rating_Data_Service/src/main/java/com/nitish/rating_data_service/RatingDataServiceApplication.java
+++ Rating_Data_Service/src/main/java/com/nitish/rating_data_service/RatingDataServiceApplication.java
@@ -3,7 +3,7 @@ package com.nitish.rating_data_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "resource")
 public class RatingDataServiceApplication {
 
     public static void main(String[] args) {
