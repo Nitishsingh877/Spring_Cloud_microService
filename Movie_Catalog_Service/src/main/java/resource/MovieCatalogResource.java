82a3e5da38e58bafc07cc85ab2d21d00339aa799
Nitish singh <nitishkumarsingh877@gmail.com>
2025-03-17 14:33:02 +0530

    added code

--- Movie_Catalog_Service/src/main/java/resource/MovieCatalogResource.java
+++ Movie_Catalog_Service/src/main/java/resource/MovieCatalogResource.java
@@ -1,9 +1,10 @@
 package resource;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import models.CatalogItem;
 import models.Movie;
-import models.Rating;
 import models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -14,7 +15,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,37 +32,31 @@ public class MovieCatalogResource {
     private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
+    @CircuitBreaker(name = "MovieCatalogService", fallbackMethod = "getFallBackCatalog")
+    @Retry(name = "MovieCatalogService")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
-    //    WebClient.Builder builder = WebClient.builder();
-
-       // RestTemplate restTemplate = new RestTemplate();
-        // Movie movie = restTemplate.getForObject("http://localhost:8081/movies/foo", Movie.class);
-
-//        List<Rating> ratings = Arrays.asList(
-//                new Rating("1234", 4),
-//                new Rating("2345", 3),
-//                new Rating("1258", 5)
-//        );
-
        UserRating ratings = restTemplate.getForObject("http://Rating-Data-Service/ratingdata/users/"+userId, UserRating.class);
 
         return ratings.getRatings().stream().map(rating -> {
                     Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+                    return  new CatalogItem(movie.getTitle(), "description", rating.getRating());
+                })
+                .collect(Collectors.toList());
+    }
+    public List<CatalogItem> getFallBackCatalog(@PathVariable("userId") String userId, Throwable throwable) {
+        System.out.println("Fallback triggered due to: " + throwable.getMessage());
+        return Arrays.asList(new CatalogItem("No Movie Found", "No Movie Found", 0));
+    }
 
+}
+
+
+//web client
 //                  Movie movie =  webClientBuilder.build()
 //                            .get()
 //                            .uri("http://localhost:8081/movies/"+rating.getMovieId())
 //                            .retrieve()
 //                            .bodyToMono(Movie.class)
 //                            .block();
-                    return  new CatalogItem(movie.getTitle(), "description", rating.getRating());
-                })
-                .collect(Collectors.toList());
 
 
-//        return Collections.singletonList(
-//                new CatalogItem("ye dil mange more","Romance",4)
-//        );
-    }
-}
